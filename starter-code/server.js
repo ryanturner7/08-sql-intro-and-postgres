e982564d904ab5c4ead26673c05ab20653d96fc3
Devin G <engy100x@gmail.com>
2017-05-17 14:15:57 -0700

    setup conString different from partner, since Im on linux

--- starter-code/server.js
+++ starter-code/server.js
@@ -12,14 +12,16 @@ const app = express();
 // TODO: Complete the connection string for the url that will connect to your local postgres database
 // Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
 // Your url may require that it's composed of additional information including user and password
-// const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
+
+// This is commented
+// const conString = 'postgres://postgres:1234@HOST:5432/hackerIpsum';
 const conString = 'postgres://localhost:5432';
 
 // TODO: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
 //       This is how it knows the URL and, for Windows and Linux users, our username and password for our
 //       database when client.connect is called on line 26. Thus, we need to pass our conString into our
 //       pg.Client() call.
-const client = new pg.Client('something needs to go here... read the instructions above!');
+const client = new pg.Client(conString);
 
 // REVIEW: Use the client object to connect to our DB.
 client.connect();
